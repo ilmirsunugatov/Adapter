396468ee6e3215ce4286464e12b7e14552cf781a
Ilmir <sunugatoffi@yandex.ru>
2023-01-23 01:15:39 +0300

    Adapter homeWork

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Ints calc = new IntsCalculator();
+        System.out.println(calc.sum(520, 16));
+        System.out.println(calc.mult(133, 15));
+        System.out.println(calc.pow(7, 19));
+    }
+}
